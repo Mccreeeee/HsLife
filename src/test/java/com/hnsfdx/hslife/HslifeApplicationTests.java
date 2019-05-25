732f186990359e9ccc48b8ed4b92f6d3583daa07
Mccreeeee <huqingyu18@163.com>
2019-05-25 18:31:29 +0800

    Initialize the hslife project

---
+++ src/test/java/com/hnsfdx/hslife/HslifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hnsfdx.hslife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HslifeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
