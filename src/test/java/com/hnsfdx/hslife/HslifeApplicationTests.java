862e7d3a542a2fdb7d2b339a92eb76c1fc58c54b
Mccreeeee <huqingyu18@163.com>
2019-06-01 23:15:22 +0800

    Modify database configuration

--- src/test/java/com/hnsfdx/hslife/HslifeApplicationTests.java
+++ src/test/java/com/hnsfdx/hslife/HslifeApplicationTests.java
@@ -1,6 +1,5 @@
 package com.hnsfdx.hslife;
 
-import org.jasypt.encryption.StringEncryptor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,17 +9,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class HslifeApplicationTests {
-    @Autowired
-    private StringEncryptor stringEncryptor;
+
 
     @Test
     void contextLoads() {
     }
     @Test
     public void encryptPwd() {
-        String password="root";//要加密的密码
-        String result = stringEncryptor.encrypt(password);
-        //打印结果
-        System.out.println(result);
+
     }
 }
