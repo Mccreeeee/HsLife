273f98cc5c31dd4178f9527cdc9b255e684348cc
ChenYuyang <cyy.chen@foxmail.com>
2019-06-02 22:39:39 +0800

    add ReponseTypeUtil.java

--- src/main/java/com/hnsfdx/hslife/controller/EntertainmentsController.java
+++ src/main/java/com/hnsfdx/hslife/controller/EntertainmentsController.java
@@ -1,9 +1,36 @@
 package com.hnsfdx.hslife.controller;
 
+import com.hnsfdx.hslife.pojo.Entertainment;
+import com.hnsfdx.hslife.service.EntertainmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ExecutionException;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/Entertainments")
 public class EntertainmentsController {
+    private EntertainmentService entertainmentService;
+
+    @Autowired
+    public EntertainmentsController(EntertainmentService entertainmentService){
+        this.entertainmentService=entertainmentService;
+    }
+
+    @PostMapping("addEntertainment")
+    public String addEntertainment(@RequestBody Entertainment entertainment){
+        try{
+            entertainmentService.insertEntertainment(entertainment);
+            return "success";
+        }
+        catch (Exception e){
+            Logger.getLogger("Runtime.Entertainments").info(String.format("添加娱乐讨论失败:%s",e.getMessage()));
+            return "fail";
+        }
+    }
+
 }
---
+++ src/main/java/com/hnsfdx/hslife/util/ReponseTypeUtil.java
@@ -0,0 +1,6 @@
+package com.hnsfdx.hslife.util;
+
+public class ReponseTypeUtil {
+    public static String BOOLEAN_SUC="success";
+    public static String BOOLEAN_FAIL="fail";
+}
