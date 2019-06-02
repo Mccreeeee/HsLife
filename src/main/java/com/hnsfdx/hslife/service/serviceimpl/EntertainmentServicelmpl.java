98ad25e892d74f66fe0b37f8cc76e875263ac365
ChenYuyang <cyy.chen@foxmail.com>
2019-06-02 22:39:39 +0800

    add ReponseTypeUtil.java

--- src/main/java/com/hnsfdx/hslife/service/serviceimpl/EntertainmentServicelmpl.java
+++ src/main/java/com/hnsfdx/hslife/service/serviceimpl/EntertainmentServicelmpl.java
@@ -3,9 +3,11 @@ package com.hnsfdx.hslife.service.serviceimpl;
 import com.hnsfdx.hslife.pojo.Entertainment;
 import com.hnsfdx.hslife.repository.EntertainmentRepository;
 import com.hnsfdx.hslife.service.EntertainmentService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class EntertainmentServicelmpl implements EntertainmentService {
     private EntertainmentRepository entertainmentRepository;
 
