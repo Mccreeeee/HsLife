612e66c57da6e387b39943cb177dcefd393fe9ef
ChenYuyang <cyy.chen@foxmail.com>
2019-06-02 22:53:10 +0800

    update ResponseTypeUtil.java

--- src/main/java/com/hnsfdx/hslife/controller/SimpleEchoForTest.java
+++ src/main/java/com/hnsfdx/hslife/controller/SimpleEchoForTest.java
@@ -1,15 +1,16 @@
 package com.hnsfdx.hslife.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("EchoTest")
 public class SimpleEchoForTest {
     @GetMapping("echo")
     public String echo(@RequestParam("msg") String msg){
+        return msg;
+    }
+    @PostMapping("mapEcho")
+    public String mapEcho(@RequestParam("msg") String msg){
         return msg;
     }
 }
