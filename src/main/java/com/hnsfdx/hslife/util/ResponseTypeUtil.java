de0f226662a98b29342372ad1741476e2df70f37
ChenYuyang <cyy.chen@foxmail.com>
2019-06-03 12:01:26 +0800

    update ResponseTypeUtil.java

--- src/main/java/com/hnsfdx/hslife/util/ResponseTypeUtil.java
+++ src/main/java/com/hnsfdx/hslife/util/ResponseTypeUtil.java
@@ -1,6 +1,19 @@
 package com.hnsfdx.hslife.util;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ResponseTypeUtil {
     public static String BOOLEAN_SUC="success";
     public static String BOOLEAN_FAIL="fail";
+    public static Map<String,Object> createFailResponse(){
+        HashMap<String,Object> forRet = new HashMap<>();
+        forRet.put("result",BOOLEAN_FAIL);
+        return forRet;
+    }
+    public static Map<String,Object> createSucResponse(){
+        HashMap<String,Object> forRet = new HashMap<>();
+        forRet.put("result",BOOLEAN_SUC);
+        return forRet;
+    }
 }
