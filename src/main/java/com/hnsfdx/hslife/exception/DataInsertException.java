5c932485bab8c46d797af3f652fe3f0a89c14bbc
Dr.Hu <huqingyu18@163.com>
2019-06-06 01:19:27 +0800

    Add some exceptions AND handler methods

--- src/main/java/com/hnsfdx/hslife/exception/DataInsertException.java
+++ src/main/java/com/hnsfdx/hslife/exception/DataInsertException.java
@@ -4,6 +4,6 @@ package com.hnsfdx.hslife.exception;
 
 
 public class DataInsertException extends RuntimeException {
-    public static final Integer EXCEPTION_CODE = 500;
+    public static final Integer EXCEPTION_CODE = 50001;
     public static final String EXCEPTION_MESSAGE = "Data Insert Error";
 }
