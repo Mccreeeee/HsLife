177f3f065c3debbb3495c7fd326774658a55614b
Dr.Hu <huqingyu18@163.com>
2019-06-06 01:19:27 +0800

    Add some exceptions AND handler methods

---
+++ src/main/java/com/hnsfdx/hslife/exception/DataUpdateException.java
@@ -0,0 +1,6 @@
+package com.hnsfdx.hslife.exception;
+
+public class DataUpdateException extends RuntimeException{
+    public static final Integer EXCEPTION_CODE = 50002;
+    public static final String EXCEPTION_MESSAGE = "Data Update Error";
+}
