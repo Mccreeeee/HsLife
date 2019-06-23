f4a565047c4c2b87b89d7756284e533d4b79ca56
Dr.Hu <huqingyu18@163.com>
2019-06-23 17:01:01 +0800

    improved the part of exception & log AOP

--- src/main/java/com/hnsfdx/hslife/exception/ArgsIntroduceException.java
+++ src/main/java/com/hnsfdx/hslife/exception/ArgsIntroduceException.java
@@ -1,4 +1,8 @@
 package com.hnsfdx.hslife.exception;
 
-public class ArgsIntroduceException extends RuntimeException {
+public class ArgsIntroduceException extends BaseException {
+    @Override
+    public ExceptionCode getExceptionCode() {
+        return ExceptionCode.valueOf("ARGS_INTRODUCE_EXCEPTION");
+    }
 }
