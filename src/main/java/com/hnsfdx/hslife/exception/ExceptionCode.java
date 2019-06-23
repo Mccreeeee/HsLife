a762e4079d82e7b81cfd407ef862df4133de0eca
Dr.Hu <huqingyu18@163.com>
2019-06-23 11:33:47 +0800

    add new exception & add exception log handler

--- src/main/java/com/hnsfdx/hslife/exception/ExceptionCode.java
+++ src/main/java/com/hnsfdx/hslife/exception/ExceptionCode.java
@@ -1,14 +1,15 @@
 package com.hnsfdx.hslife.exception;
 
-public enum DataExceptionCode {
+public enum ExceptionCode {
     DATA_INSERT_EXCEPTION(50001, "Data Insert Error"),
     DATA_UPDATE_EXCEPTION(50002, "Data Update Error"),
-    DATA_DELETE_EXCEPTION(50003, "Data Delete Error");
+    DATA_DELETE_EXCEPTION(50003, "Data Delete Error"),
+    ARGS_INTRODUCE_EXCEPTION(50004, "Args Introduce Error");
 
     private Integer code;
     private String msg;
 
-    private DataExceptionCode(Integer code, String msg) {
+    private ExceptionCode(Integer code, String msg) {
         this.code = code;
         this.msg = msg;
     }
