0d1b7bf124ddc0b51ab9b8d71864bd40fa79f281
ChenYuyang <cyy.chen@foxmail.com>
2019-06-26 00:01:03 +0800

    SignUp接口，主要WX的API有限制

--- src/main/java/com/hnsfdx/hslife/controller/UserController.java
+++ src/main/java/com/hnsfdx/hslife/controller/UserController.java
@@ -1,11 +1,18 @@
 package com.hnsfdx.hslife.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.hnsfdx.hslife.exception.DataInsertException;
 import com.hnsfdx.hslife.pojo.User;
 import com.hnsfdx.hslife.service.UserService;
+import com.hnsfdx.hslife.util.MiniProgramConst;
 import com.hnsfdx.hslife.util.ResponseTypeUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
 
@@ -19,23 +26,55 @@ public class UserController {
         this.userService = userService;
     }
 
+    @GetMapping("/signUp")
+    public Map<String, Object> login(@RequestParam("js_code") String js_code,
+                                     @RequestParam("nickname") String username,
+                                     @RequestParam("imageUrl") String imageUrl) {
+        Map<String, Object> forRet = obtainOpenId(js_code);
+        if (forRet.containsKey("errcode") && (Integer) forRet.get("errcode") != 0) {
+            forRet.put("result", ResponseTypeUtil.BOOLEAN_FAIL);
+        } else {
+            User user = new User((String) forRet.get("openid"), username, imageUrl, 0);
+            Integer result = userService.addUser(user);
+            if (result == 0) {
+                throw new DataInsertException();
+            }
+            forRet.put("result", ResponseTypeUtil.BOOLEAN_SUC);
+        }
+        return forRet;
+    }
+
+    private Map<String, Object> obtainOpenId(String js_code) {
+        String uri = String.format(
+                "https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code",
+                MiniProgramConst.getMiniProgramAppId(),
+                MiniProgramConst.getMiniProgramSecret(),
+                js_code);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
+        String strbody = new RestTemplate().exchange(uri, HttpMethod.GET, entity, String.class).getBody();
+        JSONObject parsed = JSONObject.parseObject(strbody);
+        return (Map<String, Object>) parsed;
+    }
+
     @PostMapping("/adduser")
     public Map<String, Object> addUser(@RequestBody User user) {
         Integer result = userService.addUser(user);
         Map<String, Object> res;
-        if(result == 0){
+        if (result == 0) {
             throw new DataInsertException();
-        }else {
+        } else {
             res = ResponseTypeUtil.createSucResponse();
         }
         return res;
     }
 
     @GetMapping("/getuser")
-    public Map<String,Object> getUser(@RequestParam("openId") String openId) {
+    public Map<String, Object> getUser(@RequestParam("openId") String openId) {
         User user = userService.getUser(openId);
-        Map<String,Object> forRet =  ResponseTypeUtil.createSucResponse();
-        forRet.put("data",user);
+        Map<String, Object> forRet = ResponseTypeUtil.createSucResponse();
+        forRet.put("data", user);
         return forRet;
     }
 
