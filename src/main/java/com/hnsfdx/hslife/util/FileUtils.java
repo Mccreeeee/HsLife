33bbc70c54cc6736e1864574f52a5e895e6e9e2d
Dr.Hu <huqingyu18@163.com>
2019-07-03 17:47:25 +0800

    add new exception & the operation of delete file

--- src/main/java/com/hnsfdx/hslife/util/FileUtils.java
+++ src/main/java/com/hnsfdx/hslife/util/FileUtils.java
@@ -1,5 +1,7 @@
 package com.hnsfdx.hslife.util;
 
+import com.hnsfdx.hslife.exception.ArgsIntroduceException;
+import com.hnsfdx.hslife.exception.AuthException;
 import com.hnsfdx.hslife.exception.StorageException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.multipart.MultipartFile;
@@ -9,9 +11,13 @@ import java.io.IOException;
 
 public class FileUtils {
     private static final String BASE_DIR = "temp/images/";
+
     // 相对路径（暂定为reviewerId + xxxId） + 文件名用于存储
     public static void uploadToServer(String relativePath, MultipartFile multipartFile) {
-        if(multipartFile.isEmpty()) {
+        if ("".equals(relativePath) || relativePath == null) {
+            throw new ArgsIntroduceException();
+        }
+        if (multipartFile.isEmpty() || multipartFile == null) {
             throw new StorageException();
         }
         String fileName = multipartFile.getOriginalFilename();
@@ -26,6 +32,24 @@ public class FileUtils {
             multipartFile.transferTo(file);
         } catch (IOException e) {
             throw new StorageException();
+        }
+    }
+
+    // 删除文件夹相对路径下的所有图片文件
+    public static void deleteInServer(String relativePath) {
+        if ("".equals(relativePath) || relativePath == null) {
+            throw new ArgsIntroduceException();
+        }
+        File file = new File(BASE_DIR + relativePath);
+        if (!file.isDirectory()) {
+            throw new ArgsIntroduceException();
+        }
+        // 可能会出现访问权限的异常，使用的时候再捕获异常
+        File[] listFiles = file.listFiles();
+        if (listFiles != null && listFiles.length != 0) {
+            for (File tmpFile : listFiles) {
+                tmpFile.delete();
+            }
         }
     }
 }
